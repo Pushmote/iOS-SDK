22a36213004cd73e3fd2a574246827f67cb4f81d
Taha Ozket <tahaozket@gmail.com>
2015-07-23 12:05:15 +0300

    PushmoteSDK v2

--- PushmoteSDK/Headers/Pushmote.h
+++ PushmoteSDK/Headers/Pushmote.h
@@ -1,26 +1,27 @@
+//
+//  Pushmote.h
+//  PushmoteSDK
+//
+//  Created by Taha Ozket on 28/01/15.
+//  Copyright (c) 2015 Pushmote. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
-#import <CoreLocation/CoreLocation.h>
 #import <UIKit/UIKit.h>
-#import <CoreBluetooth/CoreBluetooth.h>
 
 @class Pushmote;
 
-@protocol PSHDelegate
--(void)beaconHere;
+@protocol PushmoteActionDelegate
+-(void)actionTriggered;
 @end
 
-@interface Pushmote : NSObject <CBPeripheralDelegate> {
-    dispatch_queue_t backgroundQueue;
+
+@interface Pushmote : NSObject {
+    id <PushmoteActionDelegate> delegate;
 }
 
-@property (assign, nonatomic) BOOL isEditMode;
-@property (nonatomic, assign) id delegate;
-@property (nonatomic, strong) NSString*         userIdentifier;
+@property (nonatomic, assign) id <PushmoteActionDelegate> delegate;
 
-+ (void)startWithApplicationKey:(NSString *)applicationKey;
-+ (Pushmote *)pushmote;
-- (void)beaconHere;
-- (void)sendLogEvent:(NSString *)eventName :(NSDictionary *)eventParams;
-+ (void)enterBackground;
-
++ (void)startWithApplicationId:(NSString*)appId;
++ (void)handleNotification:(UILocalNotification*)notification;
 @end
